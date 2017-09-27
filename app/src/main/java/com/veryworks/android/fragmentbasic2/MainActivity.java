0fa1fffa67c3cd64e7d7bfb6c0e2ad07a0492cbc
javafa <nigne2016@gmail.com>
2017-09-27 14:47:13 +0900

    add CustomAdapter

--- app/src/main/java/com/veryworks/android/fragmentbasic2/MainActivity.java
+++ app/src/main/java/com/veryworks/android/fragmentbasic2/MainActivity.java
@@ -1,5 +1,6 @@
 package com.veryworks.android.fragmentbasic2;
 
+import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+    }
+
+    private void init(){
+        if(getResources().getConfiguration().orientation
+                == Configuration.ORIENTATION_PORTRAIT){ // 현재 레이아웃? 세로체크
+            initFragment();
+        }
+    }
+
+    private void initFragment(){ // 프래그먼트 더하기
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.container, new ListFragment())
+                .commit();
     }
 }
